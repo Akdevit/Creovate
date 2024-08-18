30fc54877d4d4082b4d194f879e73943a791ee3e
Abhishek kumar <128677325+Akdevit@users.noreply.github.com>
2024-08-18 13:44:40 +0530

    navbaar responsive

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { RxCross1 } from "react-icons/rx";
+
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <>
             <div className='w-full h-16 bg-gray-100 flex justify-between items-center px-4'>
-                {/* name site */}
+                {/* Site name */}
                 <div className='w-auto h-auto'>
                     <h1 className='font-bold'>Site name icons here</h1>
                 </div>
-                {/* links */}
-                <ul className='flex gap-4'>
+                {/* Links */}
+                <ul className='hidden lg:flex gap-4'>
                     <li>
                         <Link to='/'>Home</Link>
                     </li>
@@ -22,13 +31,11 @@ const Header = () => {
                     <li>
                         <Link to='/blob'>Blob Maker</Link>
                     </li>
-
                     <li>
-                        <Link to='/waves'>Waves Genratore</Link>
+                        <Link to='/waves'>Waves Generator</Link>
                     </li>
-
                     <li>
-                        <Link to='/shadow'>Shadow Genratore</Link>
+                        <Link to='/shadow'>Shadow Generator</Link>
                     </li>
                     <li>
                         <Link to='/glass'>Glass UI</Link>
@@ -37,15 +44,53 @@ const Header = () => {
                         <Link to='/colorpicker'>Color Picker</Link>
                     </li>
                     <li>
-                        <Link to='/Qr'>QR Genratore</Link>
+                        <Link to='/Qr'>QR Generator</Link>
                     </li>
-
-
                 </ul>
-                {/* add buttons profile icons  */}
+                {/* Hamburger menu */}
+                <div className='lg:hidden'>
+                    <button onClick={toggleMenu} className='focus:outline-none'>
+                        {isOpen ? <FaTimes className='w-6 h-6' /> : <FaBars className='w-6 h-6' />}
+                    </button>
+                </div>
+            </div>
+            {/* Mobile Menu */}
+            <div className={`fixed top-0 right-0 w-full z-50 h-full bg-gray-100 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out lg:hidden`}>
+                <div className='w-full h-auto flex justify-end p-8'>
+                    <RxCross1 className='text-3xl cursor-pointer' onClick={toggleMenu} />
+                </div>
+                <ul className='flex flex-col items-center gap-4 mt-16'>
+                    <li>
+                        <Link to='/' onClick={toggleMenu}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/color' onClick={toggleMenu}>Color's Palettes</Link>
+                    </li>
+                    <li>
+                        <Link to='/gradient' onClick={toggleMenu}>Gradient Generator</Link>
+                    </li>
+                    <li>
+                        <Link to='/blob' onClick={toggleMenu}>Blob Maker</Link>
+                    </li>
+                    <li>
+                        <Link to='/waves' onClick={toggleMenu}>Waves Generator</Link>
+                    </li>
+                    <li>
+                        <Link to='/shadow' onClick={toggleMenu}>Shadow Generator</Link>
+                    </li>
+                    <li>
+                        <Link to='/glass' onClick={toggleMenu}>Glass UI</Link>
+                    </li>
+                    <li>
+                        <Link to='/colorpicker' onClick={toggleMenu}>Color Picker</Link>
+                    </li>
+                    <li>
+                        <Link to='/Qr' onClick={toggleMenu}>QR Generator</Link>
+                    </li>
+                </ul>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
